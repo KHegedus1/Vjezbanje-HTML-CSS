da44daf6b3078dd3fe94d8780fc47a697354e468
KICHO123 <87566754+KICHO123@users.noreply.github.com>
2021-11-29 15:21:10 +0100

    Osnovni elementi

--- ZavrsniRadPP24.sql
+++ ZavrsniRadPP24.sql
@@ -1,1 +1,40 @@
-create tavk
+drop database if exists ZavrsniRadPP24;
+create database ZavrsniRadPP24 character set utf8;
+
+use ZavrsniRadPP24;
+
+create table kupac(
+    sifra       int not null primary key auto_increment,
+    ime         varchar(50) not null,
+    prezime     varchar(50) not null,
+    oib         char(11),
+    email       varchar(50),
+    proizvod    varchar(50)
+    
+);
+
+create table proizvod(
+    sifra       int not null primary key auto_increment,
+    naziv       varchar (50),
+    cijena      decimal(18,2),
+    kolicina    int,
+    kupac       int not null
+);
+
+create table kosarica(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    brojugovora char(11) not null,
+    klijent int not null ,
+    proizvod int not null 
+);
+
+create table adresa(
+ sifra          int not null primary key auto_increment,
+ postanskibroj  varchar(50)not null,
+ ulica          varchar(50)not null,
+ kucnibroj      int not null,
+ kupac          int,
+ proizvod       int,
+);
